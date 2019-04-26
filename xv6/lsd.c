19f90553d096cb1cc77cd1dbc2f8488de5b61017
Maderana <maderanaabhishek@gmail.com>
2019-04-26 19:36:51 +0530

    a little workaround to delete container folder

--- xv6/lsd.c
+++ xv6/lsd.c
@@ -16,6 +16,25 @@ char isspace (unsigned char c)
 
   return 0;
 }
+void*
+lsdmemmove(void *dst, const void *src, uint n)
+{
+  const char *s;
+  char *d;
+
+  s = src;
+  d = dst;
+  if(s < d && s + n > d){
+    s += n;
+    d += n;
+    while(n-- > 0)
+      *--d = *--s;
+  } else
+    while(n-- > 0)
+      *d++ = *s++;
+
+  return dst;
+}
 
 char* trimwhitespace(char *str)
 {
@@ -108,6 +127,7 @@ void correctStr(char* aa, char* bb){
 char* itoa(int num)
 {
   char* out = malloc(1);
+  printf(1,"address given to char* : %d\n",out);
   if(num == 0){ out = "0";}
   else if(num == 1){ out = "1";}
   else if(num == 2){ out = "2";}
@@ -118,7 +138,48 @@ char* itoa(int num)
   else if(num == 7){ out = "7";}
   else if(num == 8){ out = "8";}
   else{ out = "9";}
+  printf(1,"value of char* : %s\n",out);
+  return out;
+}
 
+char* itoafolder(int num)
+{
+  char* out = malloc(1);
+  printf(1,"address given to char* : %d\n",out);
+  char* one = malloc(1);
+  char* two = malloc(1);
+  char* three = malloc(1);
+  char* four = malloc(1);
+  char* five = malloc(1);
+  char* six = malloc(1);
+  char* seven = malloc(1);
+  char* eight = malloc(1);
+  char* nine = malloc(1);
+  char* zero = malloc(1);
+  
+  one   = "1";
+  two   = "2";
+  three = "3";
+  four  = "4";
+  five  = "5";
+  six   = "6";
+  seven = "7";
+  eight = "8";
+  nine  = "9";
+  zero  = "0";
+
+
+  if(num == 0){ out = zero;}
+  else if(num == 1){ out = one;}
+  else if(num == 2){ out = two;}
+  else if(num == 3){ out = three;}
+  else if(num == 4){ out = four;}
+  else if(num == 5){ out = five;}
+  else if(num == 6){ out = six;}
+  else if(num == 7){ out = seven;}
+  else if(num == 8){ out = eight;}
+  else{ out = nine;}
+  printf(1,"value of char* : %s\n",out);
   return out;
 }
 
@@ -181,12 +242,16 @@ lsd()
 
 
 void
-rmFiles(char* folder)
-{
+rmFiles(int folder)
+{ 
   char* one_dot = ".";
   char* two_dot = "..";
 
-  char* path = folder;
+  char* patht = itoa(folder);
+  char* path = malloc(1);
+  lsdmemmove(path,patht,1);
+  // free(patht);
+  
   char buf[512], *p;
   int fd;
   struct dirent de;
@@ -262,16 +327,31 @@ rmFiles(char* folder)
 
 
 void
-rmdir(int id){
-    char *cont_num = itoa(id);
-    printf(1, "Folder to delete %s\n", cont_num);
+rmdir(int num){
+    printf(1,"rm : deleting folder %d\n",num);
+    if(num == 0){ unlink("/0/");}
+    else if(num == 1){ unlink("/1/");}
+    else if(num == 2){ unlink("/2/");}
+    else if(num == 3){ unlink("/3/");}
+    else if(num == 4){ unlink("/4/");}
+    else if(num == 5){ unlink("/5/");}
+    else if(num == 6){ unlink("/6/");}
+    else if(num == 7){ unlink("/7/");}
+    else if(num == 8){ unlink("/8/");}
+    else{ unlink("/9/");}
+    // for(int i=0;i<100;i++){
+    //   char *folder_num = itoafolder(id);
+    //   // printf(1, "Folder to delete %s\n", folder_num);
+      
+    //   if(unlink(folder_num) < 0){
+    //     // printf(1, "rm: %s/ failed to delete\n", folder_num);
+    //   }
+    //   else{
+    //     printf(1, "rm: deleted file %s\n", folder_num);
+    //     return;
+    //   }
+    // }
     
-    if(unlink(cont_num) < 0){
-      printf(1, "rm: %s/ failed to delete\n", cont_num);
-    }
-    else{
-      printf(1, "rm: deleted file %s\n", cont_num);
-    }
 }
 
 int createContainer(int id){
@@ -283,7 +363,7 @@ int createContainer(int id){
 int destroyContainer(int id){
   
   printf(1,"removing container %d\n", id);
-  rmFiles(itoa(id));
+  rmFiles(id);
   rmdir(id);
   return destroy_container(id) ;
 }
